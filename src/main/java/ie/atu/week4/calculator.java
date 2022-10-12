9612973ba383dac141be665321588d3a17285653
Maghnus O Donnell <g00377514@atu.ie>
2022-10-12 10:00:02 +0100

    added multiplication function

--- src/main/java/ie/atu/week4/calculator.java
+++ src/main/java/ie/atu/week4/calculator.java
@@ -1,4 +1,26 @@
 package ie.atu.week4;
 
+import java.io.Serial;
+import java.util.Scanner;
+
 public class calculator {
+
+    public static void main(String[] args){
+
+        multi();
+    }
+
+    public static void multi(){
+        int num1, num2;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value for num1: ");
+        num1 = sc.nextInt();
+
+        System.out.print("\nEnter value for num2: ");
+        num2 = sc.nextInt();
+
+        System.out.println("\nMultiplied value: " + (num1*num2));
+    }
 }
